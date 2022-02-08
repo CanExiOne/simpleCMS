16ccefaae00c3fcb2bb5a8befa33aa324f58996d
CanExiOne <canexione@gmail.com>
2022-02-08 05:29:56 +0100

    Moved footer to one place so it's not everywhere

--- app/Views/admin/pages/settings.php
+++ app/Views/admin/pages/settings.php
@@ -232,21 +232,6 @@
   </div>
   <!-- /.content-wrapper -->
 
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline font-small">
-      Designed with <strong><a href="https://adminlte.io">AdminLTE.io</a></strong>
-       & 
-      Created by <strong>VGE Sites</strong>
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2021-<?= date('Y') ?> <a href="<?= base_url() ?>"><?=esc($settings['siteName'])?></a>.</strong> All rights reserved.
-  </footer>
-</div>
-<!-- ./wrapper -->
-</body>
-
 <script>
 $(document).ready(function() {
 
--- app/Views/admin/templates/footer.php
+++ app/Views/admin/templates/footer.php
@@ -1,3 +1,18 @@
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline font-small">
+      Designed with <strong><a href="https://adminlte.io">AdminLTE.io</a></strong>
+       & 
+      Created by <strong>VGE Sites</strong>
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2021-<?= date('Y') ?> <a href="<?= base_url() ?>"><?=esc($settings['siteName'])?></a>.</strong> Wszystkie prawa zastrzeżone.
+  </footer>
+</div>
+<!-- ./wrapper -->
+</body>
+
 <!-- Bootstrap -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 
