811377a195c1f34f6748c0b88346faaedfd3ded9
CanExiOne <canexione@gmail.com>
2022-12-10 12:07:57 +0100

    Restore access to profile editing for everyone

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -7,11 +7,9 @@
 
     <div>
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-            @canany(['update-user-profile', 'update-user-profile-email'])
                 @livewire('profile.update-profile-information-form')
 
                 <x-jet-section-border />
-            @endcanany
 
             @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
                 <div class="mt-10 sm:mt-0">
